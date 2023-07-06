38bc644625e2879a8c533ca0d65fb329d29a3d95
xxkernel <135606533+xxkernel@users.noreply.github.com>
2023-07-07 01:42:01 +0600

    Add files via upload

---
+++ Lab 3/Objects_the basics/structuredClone.js
@@ -0,0 +1,16 @@
+// The call structuredClone(object) clones the object with all nested properties.
+let user = {
+    name: "John",
+    sizes: {
+      height: 182,
+      width: 50
+    }
+  };
+  
+  let clone = structuredClone(user);
+  
+  alert( user.sizes === clone.sizes ); // false, different objects
+  
+  // user and clone are totally unrelated now
+  user.sizes.width = 60;    // change a property from one place
+  alert(clone.sizes.width); // 50, not related
